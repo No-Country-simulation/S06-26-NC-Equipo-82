5929434b98996aec281547c39a88db75830aacc8
jesusgarcia2806 <jesusgarciasw@gmail.com>
2026-06-18 10:01:01 -0300

    868k0vrg4: conexion base de datos, coleccion usuarios

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,41 @@
 from fastapi import FastAPI
-
+import os
 from app.routes.users import router as users_router
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-app = FastAPI(title="AppBit API", version="1.0.0")
+# variable de entorno archivo .env
+load_dotenv()
 
+app = FastAPI(title="Plataforma de Orientación Vocacional", version="1.0.0")
 app.include_router(users_router)
+# app.include_router(users_router)
+
+# conexión URL de Atlas
+MONGO_URL = os.getenv("MONGO_URL")
+client = AsyncIOMotorClient(MONGO_URL)
+
+# base de datos
+db = client.orientacion_personal_db
+
+# colección usuarios
+users_collection = db.users
 
 
 @app.get("/")
-def root():
-    return {"message": "API funcionando"}
+async def inicio():
+    return {"mensaje": "API conectada exitosamente a MongoDB Atlas"}
+
+
+# Endpoint de prueba para traer a los usuarios de la nube
+@app.get("/usuarios")
+async def obtener_usuarios():
+    usuarios = []
+    # Buscamos en la colección 'users' y traemos los documentos
+    cursor = users_collection.find()
+    async for documento in cursor:
+        # Convertimos el _id de Mongo a string para que FastAPI no falle al mostrarlo
+        documento["_id"] = str(documento["_id"])
+        usuarios.append(documento)
+
+    return {"total": len(usuarios), "data": usuarios}
